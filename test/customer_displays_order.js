a47936458cae21d246641e7e88f7a22296f1b2c2
Mark Greenwood <mrkjgreenwood@gmail.com>
2018-12-16 07:48:40 -0800

    Fleshed out skeleton test suite

--- test/customer_displays_order.js
+++ test/customer_displays_order.js
@@ -7,5 +7,20 @@ describe("Customer displays order", () => {
   context("Given that the order is empty", () => {
     it("will show no order items");
     it("will show 0 as the total price");
+    it("will only be possible to add a beverage");
+  });
+
+  context("Given that the order contains beverages", () => {
+    it("will show one item per beverage");
+    it("will show the sum of the unit prices as total price");
+    it("will be possible to place the order");
+    it("will be possible to add a beverage");
+    it("will be possible to remove a beverage");
+    it("will be possible to change the quantity of a beverage");
+  });
+
+  context("Given that the order has pending messages", () => {
+    it("will show the pending messages");
+    it("there will be no more pending messages");
   });
 });
