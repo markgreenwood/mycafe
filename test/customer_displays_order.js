c29edfe8411c7c27a3be14e1f80b720df3d62c10
Mark Greenwood <mrkjgreenwood@gmail.com>
2018-12-16 07:39:12 -0800

    Initial commit

---
+++ test/customer_displays_order.js
@@ -0,0 +1,11 @@
+const chai = require("chai");
+const sinon = require("sinon");
+
+const { expect } = chai;
+
+describe("Customer displays order", () => {
+  context("Given that the order is empty", () => {
+    it("will show no order items");
+    it("will show 0 as the total price");
+  });
+});
