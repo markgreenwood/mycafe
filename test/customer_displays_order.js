5aab4f1c51b59358efdc4f23e44739618f28982e
Mark Greenwood <mrkjgreenwood@gmail.com>
2018-12-24 14:12:05 -0800

    Parameterized order contains beverages

--- test/customer_displays_order.js
+++ test/customer_displays_order.js
@@ -1,6 +1,4 @@
 const chai = require("chai");
-const { inspect } = require("util");
-// const beverage = require("./support/examples/beverages");
 const order = require("./support/examples/orders");
 const newStorage = require("./support/storageDouble");
 const orderSystemWith = require("../lib/orders");
@@ -45,50 +43,55 @@ describe("Customer displays order", () => {
         ]));
   });
 
-  context("Given that the order contains beverages", () => {
-    beforeEach(async () => {
-      this.order = this.orderStorage.alreadyContains(
-        order.withItems([
-          { beverage: "espresso", quantity: 1 },
-          { beverage: "mochaccino", quantity: 2 },
-        ])
-      );
-      this.orderActions = order.actionsFor(this.order);
-      this.result = this.orderSystem.display(this.order.id);
+  const scenarioOrderContainsBeverages = testExample => {
+    context(`Given that the order contains ${testExample.title}`, () => {
+      beforeEach(async () => {
+        this.order = this.orderStorage.alreadyContains(order.withItems(testExample.items));
+        this.orderActions = order.actionsFor(this.order);
+        this.result = this.orderSystem.display(this.order.id);
+      });
+
+      it("will show one item per beverage", () =>
+        expect(this.result)
+          .to.eventually.have.property("items")
+          .that.is.deep.equal(this.order.data));
+
+      it("will show the sum of the unit prices as total price", () =>
+        expect(this.result)
+          .to.eventually.have.property("totalPrice")
+          .that.is.equal(testExample.expectedTotalPrice));
+
+      it("will be possible to place the order", async () =>
+        expect(this.result)
+          .to.eventually.have.property("actions")
+          .that.deep.include(this.orderActions.place()));
+
+      it("will be possible to add a beverage", () =>
+        expect(this.result)
+          .to.eventually.have.property("actions")
+          .that.deep.include(this.orderActions.appendItem()));
+
+      it("will be possible to remove a beverage", () =>
+        expect(this.result)
+          .to.eventually.have.property("actions")
+          .that.deep.include(this.orderActions.removeItem(0))
+          .and.that.deep.include(this.orderActions.removeItem(1)));
+
+      it("will be possible to change the quantity of a beverage", () =>
+        expect(this.result)
+          .to.eventually.have.property("actions")
+          .that.deep.include(this.orderActions.editItemQuantity(0))
+          .and.that.deep.include(this.orderActions.editItemQuantity(1)));
     });
+  };
 
-    it("will show one item per beverage", () =>
-      expect(this.result)
-        .to.eventually.have.property("items")
-        .that.is.deep.equal(this.order.data));
-
-    it("will show the sum of the unit prices as total price", () =>
-      expect(this.result)
-        .to.eventually.have.property("totalPrice")
-        .that.is.equal(6.1));
-
-    it("will be possible to place the order", async () =>
-      expect(this.result)
-        .to.eventually.have.property("actions")
-        .that.deep.include(this.orderActions.place()));
-
-    it("will be possible to add a beverage", () =>
-      expect(this.result)
-        .to.eventually.have.property("actions")
-        .that.deep.include(this.orderActions.appendItem()));
-
-    it("will be possible to remove a beverage", () =>
-      expect(this.result)
-        .to.eventually.have.property("actions")
-        .that.deep.include(this.orderActions.removeItem(0))
-        .and.that.deep.include(this.orderActions.removeItem(1)));
-
-    it("will be possible to change the quantity of a beverage", () =>
-      expect(this.result)
-        .to.eventually.have.property("actions")
-        .that.deep.include(this.orderActions.editItemQuantity(0))
-        .and.that.deep.include(this.orderActions.editItemQuantity(1)));
-  });
+  [
+    {
+      title: "1 Espresso and 2 Mochaccinos",
+      items: [{ beverage: "espresso", quantity: 1 }, { beverage: "mochaccino", quantity: 2 }],
+      expectedTotalPrice: 6.1,
+    },
+  ].forEach(scenarioOrderContainsBeverages);
 
   context("Given that the order has pending messages", () => {
     it("will show the pending messages");
